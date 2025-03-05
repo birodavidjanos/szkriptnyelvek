463889f0d0cf348bab2a31a41043e6cd4455adf8
birodavidjanos <biro.david.janos@gmail.com>
2025-03-05 22:09:55 +0100

    Update string_form.py

--- házi1/string_form.py
+++ házi1/string_form.py
@@ -1,22 +1,3 @@
-"""lang_info =  [('Fortran', 1954, 0.435), ('Cobol', 1959, 0.391),
-              ('C', 1972, 16.076), ('C++', 1980, 9.014), 
-              ('Python', 1991, 6.482), 
-              ('Java', 1995, 17.99), ('C#', 2001, 6.687)]
-
-print "Language Year Developed TIOBE rating"
-print "--------------------------------------"
-for element in lang_info:
-    print element[0], element[1], element[2]
-
-print "{0} {1} {2}".format("Language", "Year Developed",
-                            "TIOBE rating")
-print "-"*46
-for element in lang_info:
-    print "{0} {1} {2}".format(element[0], element[1], element[2])
-"""
-
-
-
 lang_info = [
     ('Fortran', 1954, 0.435), ('Cobol', 1959, 0.391),
     ('C', 1972, 16.076), ('C++', 1980, 9.014),
@@ -24,7 +5,13 @@ lang_info = [
     ('C#', 2001, 6.687)
 ]
 
-#[[fill]align][sign][#][0][minimumwidth][.precision][type]
+"""[[fill]align][sign][#][0][minimumwidth][.precision][type]
+A formázási specifikátorok (<, ^, >, 16.2f) az alábbiakat jelentik:
+    <12: Balra igazított, 12 karakter széles mező.
+    ^16: Középre igazított, 16 karakter széles mező.
+    >16.2f: Jobbra igazított, 16 karakter széles mező, és két tizedesjegyre kerekített lebegőpontos szám.
+"""
+
 print("{0:<12} {1:^16} {2:>16}".format("Language", "Year Developed", "TIOBE rating"))
 print("-" * 46)
 
